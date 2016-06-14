fdc1b339b32c2126655b7dd998733d1129a209a7
cmptech <wanjochan@gmail.com>
2016-06-14 12:44:25 +0800

    com.android.support:appcompat is no need

--- szu.bdi.hybrid.coretest/src/main/java/szu/bdi/hybrid/coretest/CoreTestApp.java
+++ szu.bdi.hybrid.coretest/src/main/java/szu/bdi/hybrid/coretest/CoreTestApp.java
@@ -3,13 +3,16 @@ package szu.bdi.hybrid.coretest;
 import android.app.Application;
 import android.content.Context;
 import android.os.Build;
-import android.os.Handler;
 import android.os.StrictMode;
 import android.util.Log;
 
 import szu.bdi.hybrid.core.HybridTools;
 
-//import com.hybrid.core.UiRoot;
+/**
+ * For core test, it only do basic test about WebView/JsBridge/Ui-Open/Ui-Close
+ * For dynamic reflection based on "config.json" to link api and ui to class,
+ * please refer to app-hybrid-demo for more detail.
+ */
 
 public class CoreTestApp extends Application {
     final private static String LOGTAG = "" + (new Object() {
@@ -26,13 +29,7 @@ public class CoreTestApp extends Application {
         HybridTools.setAppContext(_ctx);
 
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-            new Handler().postDelayed(new Runnable() {
-                public void run() {
-                    HybridTools.quickShowMsg(getApplicationContext(),
-                            "Warning: Your Phone is too old (API " + Build.VERSION.SDK_INT + ")... May have some problem...");
-//                    HybridTools.sendRemoteLog();
-                }
-            }, 1000);
+            HybridTools.quickShowMsgMain("Warning: Your Phone is too old (API " + Build.VERSION.SDK_INT + ")... May have some problem...");
         }
 
         //NOTES: for main thread (could) using network, do a policy config hack:
